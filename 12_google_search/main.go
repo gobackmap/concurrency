951834ef8faeaa674fb999baf606e62bbd93abc2
Tirouzh <mohammad.kolagar@gmail.com>
2022-09-09 06:22:55 +0430

    Add google search example (v3.0)

--- 12_google_search/main.go
+++ 12_google_search/main.go
@@ -6,12 +6,15 @@ import (
 	"time"
 )
 
-var versions []string = []string{"v1.0", "v2.0", "v2.1"}
+var versions = []string{"v1.0", "v2.0", "v2.1", "v3.0"}
 
 var (
-	Web   = fakeSearch("web")
-	Image = fakeSearch("image")
-	Video = fakeSearch("video")
+	Web    = fakeSearch("web")
+	Web2   = fakeSearch("web")
+	Image  = fakeSearch("image")
+	Image2 = fakeSearch("image")
+	Video  = fakeSearch("video")
+	Video2 = fakeSearch("video")
 )
 
 type Result string
@@ -23,10 +26,17 @@ func main() {
 		start := time.Now()
 		results := Google("golang", version)
 		elapsed := time.Since(start)
-		fmt.Printf("----------%s----------\n", version)
+		fmt.Printf("--------------------%s--------------------\n", version)
 		fmt.Println(results)
 		fmt.Println("elapsed time:", elapsed)
 	}
+	// Using the first function
+	start := time.Now()
+	results := First("golang", fakeSearch("replica 1"), fakeSearch("replica 2"))
+	elapsed := time.Since(start)
+	fmt.Println("----------Using The First Function----------")
+	fmt.Println(results)
+	fmt.Println("elapsed time:", elapsed)
 }
 
 func fakeSearch(kind string) Search {
@@ -72,6 +82,35 @@ func Google(query string, version string) (results []Result) {
 				return
 			}
 		}
+	case "v3.0":
+		// Reduce tail latency using replicated search servers.
+		c := make(chan Result)
+		go func() { c <- First(query, Web, Web2) }()
+		go func() { c <- First(query, Image, Image2) }()
+		go func() { c <- First(query, Video, Video2) }()
+
+		timeout := time.After(80 * time.Millisecond)
+
+		for i := 0; i < 3; i++ {
+			select {
+			case result := <-c:
+				results = append(results, result)
+			case <-timeout:
+				fmt.Println("timed out")
+				return
+			}
+		}
 	}
 	return
+}
+
+// First replicates the servers to avoid discarding results from slow ones.
+// It sends requests to multiple replicas, and uses the first response.
+func First(query string, replicas ...Search) Result {
+	c := make(chan Result)
+	searchReplica := func(i int) { c <- replicas[i](query) }
+	for i := range replicas {
+		go searchReplica(i)
+	}
+	return <-c
 }
