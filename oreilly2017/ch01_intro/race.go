795135a200d2713da22b036124e071120b1fbcb0
Tirouzh <mohammad.kolagar@gmail.com>
2022-09-13 00:39:59 +0430

    Refactor race example

--- oreilly2017/ch01_intro/race.go
+++ oreilly2017/ch01_intro/race.go
@@ -6,12 +6,18 @@ import (
 	"github.com/fatih/color"
 )
 
+var (
+	fgGreen = color.New(color.FgGreen)
+	fgBlue  = color.New(color.FgBlue, color.Underline)
+)
+
 func Race() {
-	const N int = 50
-	var data int
+	numReadingTry := 50
+	var sharedData int
+
 	fmt.Println("==============================================")
 	fmt.Println(" Race Condition Exmample")
-	fmt.Printf(" Number of Iteration=%d, shared data=%d\n", N, data)
+	fmt.Printf(" Number of trying to read=%d, shared data=%d\n", numReadingTry, sharedData)
 	fmt.Println(" Three possible outcomes:")
 	fmt.Println("   -Nothing to print")
 	fmt.Println("   -data=0")
@@ -19,17 +25,15 @@ func Race() {
 	fmt.Println(" Just run the program again and again!")
 	fmt.Println("==============================================")
 
-	fgGreen := color.New(color.FgGreen)
-	fgBlue := color.New(color.FgBlue, color.Underline)
 	go func() {
 		fgGreen.Println("\ngoroutine >>> the value of data is going to increase...")
-		data++
+		sharedData++
 		fgGreen.Println("goroutine >>> the value of data increased...")
 	}()
-	for i := 0; i < N; i++ {
+	for i := 0; i < numReadingTry; i++ {
 		fmt.Printf(" iteration %d... ", i)
-		if data == 0 {
-			fgBlue.Printf("| it=%d, data=%v |", i, data)
+		if sharedData == 0 {
+			fgBlue.Printf("| it=%d, data=%v |", i, sharedData)
 		}
 	}
 	fmt.Println()
