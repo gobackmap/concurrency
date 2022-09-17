ce9ec0bf804de21011cce462c4460bcf4d3ec38b
Tirouzh <mohammad.kolagar@gmail.com>
2022-09-17 05:56:10 +0430

    Refctor main.go

--- oreilly2017/main.go
+++ oreilly2017/main.go
@@ -14,19 +14,30 @@ type Chapter struct {
 	topics []Topic
 }
 
-type Topic string
+type Topic struct {
+	title       string
+	description string
+	callback    func()
+}
 
 var Chapters = []Chapter{
-	{1, "intro", []Topic{"race"}},
+	{1, "intro", []Topic{
+		{"race", "race condition", intro.Race},
+		{"sync", "memory access synchronization", intro.MemoryAccessSynchronization},
+		{"deadlock", "deadlock", intro.Deadlock},
+		{"livelock", "livelock", intro.Livelock},
+	}},
 	{2, "csp", []Topic{}},
 }
 
+var args []string
+
 func main() {
-	args := os.Args
+	args = os.Args
 	if len(args) > 1 {
 		switch args[1] {
 		case Chapters[0].title:
-			intro.Race()
+			checkTopic(Chapters[0])
 		case Chapters[1].title:
 			log.Fatal("not implemented")
 		default:
@@ -34,6 +45,18 @@ func main() {
 		}
 	} else {
 		chapterHelp()
+	}
+}
+
+func checkTopic(chapter Chapter) {
+	if len(args) > 2 {
+		for _, topic := range chapter.topics {
+			if topic.title == args[2] {
+				topic.callback()
+			}
+		}
+	} else {
+		topicHelp(chapter)
 	}
 }
 
@@ -45,6 +68,19 @@ func chapterHelp() {
 			fmt.Printf("%s", ch.title)
 		default:
 			fmt.Printf("%s|", ch.title)
+		}
+	}
+	fmt.Println(">")
+}
+
+func topicHelp(chapter Chapter) {
+	fmt.Printf("Specify a topic using\n 	go run . %s <", chapter.title)
+	for i, topic := range chapter.topics {
+		switch i {
+		case len(chapter.topics) - 1:
+			fmt.Printf("%s", topic.title)
+		default:
+			fmt.Printf("%s|", topic.title)
 		}
 	}
 	fmt.Println(">")
