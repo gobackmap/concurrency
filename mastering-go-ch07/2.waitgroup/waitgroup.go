9e796fe7ba841d1f43d42ff01e41e68d70e21960
tirouzh <ctirouzh@gmail.com>
2023-06-11 00:18:51 +0330

    Using wait group to create multiple test files

--- mastering-go-ch07/2.waitgroup/waitgroup.go
+++ mastering-go-ch07/2.waitgroup/waitgroup.go
@@ -4,38 +4,59 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"runtime"
 	"strconv"
 	"sync"
 )
 
-func main() {
-	// Multiple goroutines
-	var (
-		count int
-		err   error
-		wg    sync.WaitGroup
-	)
-	if len(os.Args) > 1 {
-		count, err = strconv.Atoi(os.Args[1])
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("Going to create %d goroutines...\n", count)
-	}
-	if count == 0 {
-		log.Println("at least one positive integer arg is required!")
-	}
+var (
+	dir      = "./mastering-go-ch07/2.waitgroup/tmp"
+	filename string
+)
 
-	for i := 0; i < count; i++ {
+var (
+	start, end int
+	err        error
+	wg         sync.WaitGroup
+)
+
+func main() {
+	for i := start; i <= end; i++ {
 		wg.Add(1)
-		go func(x int) {
+		name := fmt.Sprintf("%d.%s", i, filename)
+		go func(fName string) {
 			defer wg.Done()
-			fmt.Printf("%d ", x)
-		}(i)
+			if wErr := os.WriteFile(dir+"/"+fName, []byte("This is a test file"), 0666); wErr != nil {
+				log.Println("failed to write file:", dir+"/"+fName)
+			}
+		}(name)
 	}
-	fmt.Printf("Waiting for %d number of goroutines to be done...\nNumber of goroutines: %d\n", count, runtime.NumGoroutine())
 	wg.Wait()
-	fmt.Printf("\nDone...only %d goroutine remained: main goroutine!\n", runtime.NumGoroutine())
 	fmt.Println("Exit...")
+}
+
+func init() {
+	if len(os.Args) != 4 {
+		log.Fatal("Usage: go run .../waitgroup.go startInt endInt filename")
+	}
+	for i, v := range os.Args {
+		switch i {
+		case 1:
+			start, err = strconv.Atoi(v)
+			if err != nil {
+				log.Fatal(err)
+			}
+		case 2:
+			end, err = strconv.Atoi(v)
+			if err != nil {
+				log.Fatal(err)
+			}
+		case 3:
+			filename = v
+		}
+	}
+	if err = os.Mkdir(dir, 0750); err != nil && !os.IsExist(err) {
+		log.Fatal(err)
+	}
+	fmt.Printf("Going to create %d goroutines for each file creation...\n", start-end+1)
+
 }
